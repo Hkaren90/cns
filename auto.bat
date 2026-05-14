4b4b4a885e257f4cb933530474880e93d06c8b3f
Hkaren90 <hkaren0205@gmail.com>
2026-05-15 00:32:56 +0530

    Update auto.bat

--- auto.bat
+++ auto.bat
@@ -4,15 +4,22 @@ setlocal enabledelayedexpansion
 :menu
 cls
 echo.
-echo "enter the exp number"
+echo =========================
+echo     CNS PROGRAM VIEWER
+echo =========================
+echo.
 echo 0. Exit
 echo.
+
 set /p exp=Enter Experiment Number: 
 
 if "%exp%"=="0" exit
 if "%exp%"=="" goto menu
-if %exp% LSS 0 goto menu
-if %exp% GTR 10 goto menu
+
+echo %exp% | findstr /r "^[0-9][0-9]*$" >nul || goto menu
+
+if %exp% LSS 1 goto menu
+if %exp% GTR 12 goto menu
 
 cls
 echo =========================
@@ -20,9 +27,10 @@ echo Experiment %exp%
 echo =========================
 echo.
 
-curl -s https://api.github.com/repos/Hkaren90/react/contents/%exp% > temp.txt
+curl -s https://api.github.com/repos/Hkaren90/cns/contents/%exp% > temp.txt
 
 for /f "tokens=2 delims=:," %%a in ('findstr /i "\"name\"" temp.txt') do (
+
     set file=%%~a
     set file=!file:"=!
     set file=!file: =!
@@ -30,11 +38,15 @@ for /f "tokens=2 delims=:," %%a in ('findstr /i "\"name\"" temp.txt') do (
     echo ==================================
     echo !file!
     echo ==================================
-    curl -sL https://raw.githubusercontent.com/Hkaren90/react/main/%exp%/!file!
+    echo.
+
+    curl -sL https://raw.githubusercontent.com/Hkaren90/cns/main/%exp%/!file!
+
     echo.
     echo.
 )
 
 del temp.txt
+
 pause
 goto menu
